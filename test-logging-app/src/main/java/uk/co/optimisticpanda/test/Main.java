445f585e2b66c33f9f66278eab2e592fb8df3093
Andy L <plasma147@gmail.com>
2019-01-13 10:34:08 +0000

    Updating amazon dependency
    Splitting project into a multimodule app with a sample application
    module.
    
    Moving dependency to sns-sdk rather than pulling in the entire sdk, also
    updating to the latest version.
    
    Also moving to gradle

---
+++ test-logging-app/src/main/java/uk/co/optimisticpanda/test/Main.java
@@ -0,0 +1,17 @@
+package uk.co.optimisticpanda.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static final Logger L = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+
+        L.info("Hello world");
+
+    }
+
+}
